943619159243c20f1d99b75a566b23b1eebe38ce
beitaqa25 <mari01beita@gmail.com>
2024-08-07 20:47:42 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let parts = {
+  who: ["The dog", "My grandma", "The mailman", "My bird"],
+  action: ["ate", "peed", "crushed", "broke"],
+  what: ["my homework", "my phone", "the car"],
+  when: ["before the class", "when I was sleeping", "while I was exercising","during my lunch"]
 };
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function generateRandomExcuse() {
+  return `${getRandomElement(parts.who)} ${getRandomElement(
+    parts.action
+  )} ${getRandomElement(parts.what)} ${getRandomElement(parts.when)}`;
+}
+
+function displayExcuse() {
+  document.getElementById("excuse").textContent = generateRandomExcuse();
+}
+
+window.onload = displayExcuse;
